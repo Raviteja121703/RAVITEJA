f781669596960841f8af5ad66c0c80ce1c18bf4b
raviteja <raviteja.c@navayuvabharatinfotech.com>
2025-03-27 17:18:10 +0530

    datatypes

---
+++ .vscode/SQL/sql table.sql
@@ -0,0 +1,11 @@
+ CREATE TABLE tollywood (
+    ser_no      NUMBER,
+    hero        VARCHAR(100),
+    heroine     VARCHAR(100),
+    director    VARCHAR(100),
+    movie       VARCHAR(100),
+    year        NUMBER,
+    collections NUMBER
+);
+     
+     desc tollywood;
